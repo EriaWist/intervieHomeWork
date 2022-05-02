da445d78dd0651c2f10c0d6aac954fc446ffb8f9
EriaWist <c121680118@gmail.com>
2022-05-02 16:15:08 +0800

    init

---
+++ interviewHomeWork/interviewHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  interviewHomeWork
+//
+//  Created by 阿騰 on 2022/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
